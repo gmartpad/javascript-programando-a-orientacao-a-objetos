08910534a95b87fb07a3c5d56e3a794d7c07ba16
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-12-12 18:33:27 -0300

    Entendendo o problema do cliente

---
+++ index.js
@@ -0,0 +1,11 @@
+const cliente1Nome = "José";
+const cliente1CPF = 11122233309;
+const cliente1Agencia = 1001;
+const cliente1Saldo = 0;
+
+const cliente2Nome = "Joana";
+const cliente2CPF = 88822233309;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+console.log(cliente1Nome);
