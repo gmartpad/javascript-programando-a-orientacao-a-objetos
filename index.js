dd2691e735e4ae8e991c53fd951526300ba19e93
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-12-12 20:05:20 -0300

    Comportamentos de classes

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 class Cliente {
     nome;
     cpf;
+}
+
+class ContaCorrente {
     agencia;
     saldo;
+
+    sacar(valorSacado){
+        if(this.saldo >= valorSacado){
+            this.saldo -= valorSacado;
+        }
+    }
 }
 
 const cliente1 = new Cliente();
 
-cliente1.nome = "José";
+cliente1.nome = "Jose";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
 const cliente2 = new Cliente();
 
 cliente2.nome = "Joana";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
-console.log(cliente1, cliente2);
+const contaCorrenteJose = new ContaCorrente();
+
+contaCorrenteJose.saldo = 0;
+contaCorrenteJose.agencia = 1001;
+
+console.log(contaCorrenteJose.saldo);
+contaCorrenteJose.saldo += 100;
+console.log(contaCorrenteJose.saldo);
+contaCorrenteJose.sacar(100);
+console.log(contaCorrenteJose.saldo);
+
+
+
+console.log(cliente1);
+console.log(cliente2);
